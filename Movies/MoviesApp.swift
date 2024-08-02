66b3a67abcc681b6d7671b5db743b8d4d0d613c2
Rodrigo Galeano <rodrigogaleano@jera.com.br>
2024-08-01 20:49:26 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Rodrigo Galeano on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
